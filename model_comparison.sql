540aac7bfaef94c7c8e1eaa3a85188df7a16c830
gregcrewse <gregcrewse@gmail.com>
2025-01-26 19:58:27 -0500

    Update model_comparison.sql

--- model_comparison.sql
+++ model_comparison.sql
@@ -1,10 +1,9 @@
-{# macros/model_comparison.sql #}
 {% macro compare_models(model_name) %}
     {{ log("Starting comparison for model: " ~ model_name, info=True) }}
     
-    {% set dev_schema = 'NULL' %}
-    {% set uat_schema = 'NULL' %}
-    {% set database = 'NULL' %}
+    {% set dev_schema = target.schema %}
+    {% set uat_schema = 'uat' %}
+    {% set database = target.database %}
     
     {# Get columns from both environments #}
     {{ log("Getting DEV columns...", info=True) }}
@@ -27,39 +26,35 @@
     {% set uat_columns = adapter.get_columns_in_relation(uat_relation) %}
     {{ log("Found " ~ uat_columns|length ~ " columns in UAT", info=True) }}
     
-    {# Create maps of column names to data types #}
+    {# Create maps of column names to data types, normalizing for case sensitivity #}
     {% set dev_col_map = {} %}
     {% set uat_col_map = {} %}
     
-    {{ log("DEV columns:", info=True) }}
     {% for col in dev_columns %}
-        {% do dev_col_map.update({col.name: col.dtype}) %}
-        {{ log("  - " ~ col.name ~ " (" ~ col.dtype ~ ")", info=True) }}
+        {% do dev_col_map.update({col.name|lower: col.dtype}) %}
     {% endfor %}
     
-    {{ log("UAT columns:", info=True) }}
     {% for col in uat_columns %}
-        {% do uat_col_map.update({col.name: col.dtype}) %}
-        {{ log("  - " ~ col.name ~ " (" ~ col.dtype ~ ")", info=True) }}
+        {% do uat_col_map.update({col.name|lower: col.dtype}) %}
     {% endfor %}
+    
+    {# Log column mappings #}
+    {{ log("DEV columns map: " ~ dev_col_map, info=True) }}
+    {{ log("UAT columns map: " ~ uat_col_map, info=True) }}
     
     {# Find added and removed columns #}
     {% set added_columns = [] %}
     {% set removed_columns = [] %}
     
-    {{ log("Checking for added columns...", info=True) }}
     {% for col in uat_col_map %}
         {% if col not in dev_col_map %}
             {% do added_columns.append(col) %}
-            {{ log("  + Added: " ~ col, info=True) }}
         {% endif %}
     {% endfor %}
     
-    {{ log("Checking for removed columns...", info=True) }}
     {% for col in dev_col_map %}
         {% if col not in uat_col_map %}
             {% do removed_columns.append(col) %}
-            {{ log("  - Removed: " ~ col, info=True) }}
         {% endif %}
     {% endfor %}
     
@@ -85,7 +80,7 @@
             dev_stats.total_rows::VARCHAR as dev_total_rows,
             uat_stats.total_rows::VARCHAR as uat_total_rows
             {% for column in dev_columns %}
-                {% if column.name in uat_col_map %}
+                {% if column.name|lower in uat_col_map %}
                 , dev_stats.non_null_{{ column.name }}::VARCHAR as dev_non_null_{{ column.name }}
                 , uat_stats.non_null_{{ column.name }}::VARCHAR as uat_non_null_{{ column.name }}
                 , dev_stats.unique_{{ column.name }}::VARCHAR as dev_unique_{{ column.name }}
@@ -94,59 +89,57 @@
             {% endfor %}
         FROM dev_stats, uat_stats
     {% endset %}
-
+    
     {{ log("Running query...", info=True) }}
     {{ log(stats_query, info=True) }}
     
     {% if execute %}
-        {{ log("Executing query...", info=True) }}
         {% set results = run_query(stats_query) %}
-        {% set stats_row = results.rows[0] %}
-        
-        {% set comparison_data = {
-            'model_name': model_name,
-            'total_rows': {
-                'dev_value': stats_row.dev_total_rows,
-                'uat_value': stats_row.uat_total_rows,
-                'difference': (stats_row.uat_total_rows|int - stats_row.dev_total_rows|int)|string,
-                'percent_change': ((((stats_row.uat_total_rows|int - stats_row.dev_total_rows|int) / stats_row.dev_total_rows|int) * 100)|round(2))|string if stats_row.dev_total_rows|int > 0 else '0'
-            },
-            'columns': {},
-            'added_columns': added_columns,
-            'removed_columns': removed_columns
-        } %}
-        
-        {# Add column-level statistics #}
-        {{ log("Processing column statistics...", info=True) }}
-        {% for column in dev_columns %}
-            {% if column.name in uat_col_map %}
-                {% set dev_non_null = stats_row['dev_non_null_' ~ column.name]|int %}
-                {% set uat_non_null = stats_row['uat_non_null_' ~ column.name]|int %}
-                {% set dev_unique = stats_row['dev_unique_' ~ column.name]|int %}
-                {% set uat_unique = stats_row['uat_unique_' ~ column.name]|int %}
-                
-                {% do comparison_data.columns.update({
-                    column.name: {
-                        'data_type': column.dtype|string,
-                        'non_null_values': {
-                            'dev_value': dev_non_null|string,
-                            'uat_value': uat_non_null|string,
-                            'difference': (uat_non_null - dev_non_null)|string,
-                            'percent_change': ((((uat_non_null - dev_non_null) / dev_non_null) * 100)|round(2))|string if dev_non_null > 0 else '0'
-                        },
-                        'unique_values': {
-                            'dev_value': dev_unique|string,
-                            'uat_value': uat_unique|string,
-                            'difference': (uat_unique - dev_unique)|string,
-                            'percent_change': ((((uat_unique - dev_unique) / dev_unique) * 100)|round(2))|string if dev_unique > 0 else '0'
+        {% if results %}
+            {% set stats_row = results.rows[0] %}
+            {% set comparison_data = {
+                'model_name': model_name,
+                'total_rows': {
+                    'dev_value': stats_row.dev_total_rows,
+                    'uat_value': stats_row.uat_total_rows,
+                    'difference': (stats_row.uat_total_rows|int - stats_row.dev_total_rows|int)|string,
+                    'percent_change': ((((stats_row.uat_total_rows|int - stats_row.dev_total_rows|int) / stats_row.dev_total_rows|int) * 100)|round(2))|string if stats_row.dev_total_rows|int > 0 else '0'
+                },
+                'columns': {},
+                'added_columns': added_columns,
+                'removed_columns': removed_columns
+            } %}
+            
+            {% for column in dev_columns %}
+                {% if column.name|lower in uat_col_map %}
+                    {% set dev_non_null = stats_row['dev_non_null_' ~ column.name]|int %}
+                    {% set uat_non_null = stats_row['uat_non_null_' ~ column.name]|int %}
+                    {% set dev_unique = stats_row['dev_unique_' ~ column.name]|int %}
+                    {% set uat_unique = stats_row['uat_unique_' ~ column.name]|int %}
+                    
+                    {% do comparison_data.columns.update({
+                        column.name: {
+                            'data_type': column.dtype|string,
+                            'non_null_values': {
+                                'dev_value': dev_non_null|string,
+                                'uat_value': uat_non_null|string,
+                                'difference': (uat_non_null - dev_non_null)|string,
+                                'percent_change': ((((uat_non_null - dev_non_null) / dev_non_null) * 100)|round(2))|string if dev_non_null > 0 else '0'
+                            },
+                            'unique_values': {
+                                'dev_value': dev_unique|string,
+                                'uat_value': uat_unique|string,
+                                'difference': (uat_unique - dev_unique)|string,
+                                'percent_change': ((((uat_unique - dev_unique) / dev_unique) * 100)|round(2))|string if dev_unique > 0 else '0'
+                            }
                         }
-                    }
-                }) %}
-            {% endif %}
-        {% endfor %}
-        
-        {{ log("RESULTS_START", info=True) }}
-        {{ log("=" ~ tojson(comparison_data) ~ "=", info=True) }}
-        {{ log("RESULTS_END", info=True) }}
+                    }) %}
+                {% endif %}
+            {% endfor %}
+            
+            {{ log("RESULTS_START", info=True) }}
+            {{ log("=" ~ tojson(comparison_data) ~ "=", info=True) }}
+            {{ log("RESULTS_END", info=True) }}
+        {% endif %}
     {% endif %}
 {% endmacro %}
